2100276c02fcbba5c8a47207964874b55099a75b
Nicolas Palacio <nicolaspalacio91@gmail.com>
2024-02-22 13:10:22 +0100

    Updated documentation on DataSet class

--- src/funki/input.py
+++ src/funki/input.py
@@ -16,14 +16,42 @@ _read_ext = {
 
 
 class DataSet(anndata.AnnData):
+    '''
+    Data set class, inherits from `anndata.AnnData`_ class.
+
+    :param X: An instance of `anndata.AnnData`_ or any other class accepted by
+        `anndata.AnnData`_, defaults to ``None``.
+    :type X: anndata.AnnData, optional
+
+    .. _anndata.AnnData: https://anndata.readthedocs.io/en/latest/generated/ann\
+        data.AnnData.html
+    '''
+
     def __init__(self, X=None):
         super(self.__class__, self).__init__(X)
 
 
 def read(path, *args, **kwargs):
     '''
-    Function takes a path to a file and uses the appropiate `anndata` reading
-    function according to the file extension.
+    Function takes a path to a file and uses the appropiate `anndata reading`_
+    function according to the file extension provided in ``path``. You can find
+    further information on the available formats in the
+    `anndata reading`_ documentation. If file extension not recognized, it
+    defaults to `anndata.read_text`_.
+
+    :param path: Path to the data file.
+    :type path: str
+    :param \*args: Other positional arguments that can be required by the
+        reading function, according to the data file format.
+    :type \*args: optional
+    :param \*\*kwargs: Other keyword arguments that can be required by the
+        reading function, according to the data file format.
+    :type \*\*kwargs: optional
+
+    .. _anndata reading: https://anndata.readthedocs.io/en/latest/api.htm\
+        l\#reading
+    .. _anndata.read_text: https://anndata.readthedocs.io/en/latest/generated/a\
+        nndata.read_text.html
     '''
 
     fname, ext = os.path.splitext(path)
