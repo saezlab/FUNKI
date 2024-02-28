a4324acd1e7b87093546a016e50f64cd3565f196
Nicolas Palacio <nicolaspalacio91@gmail.com>
2024-02-28 13:45:52 +0100

    Added clustering function

--- src/funki/analysis.py
+++ src/funki/analysis.py
@@ -21,3 +21,34 @@ def sc_trans_qc_metrics(data, var_name='mito'):
                                log1p=False, inplace=True)
 
     return aux
+
+def sc_clustering(data, alg='leiden', resolution=1.0):
+    '''
+    Computes the clustering of the cells according to the selected algorithm and
+    resolution parameter. You can plot the resulting clustering with your
+    dimensionality reduction approach of choice (:func:`funki.plots.plot_pca`,
+    :func:`funki.plots.plot_tsne` or :func:`funki.plots.plot_umap`) by passing
+    the clustering algorithm name to the keyword argument ``color`` (e.g.:
+    ``plot_tsne(mydata, color='leiden')``)
+
+    :param data: A single-cell transcriptomic data set instance
+    :type data: :class:`funki.input.DataSet`
+    :param alg: The algorithm to use for computing the clusters, can be either
+        ``'leiden'`` or ``'louvain'``, defaults to ``'leiden'``
+    :type alg: str, optional
+    :param resolution: The resolution of the clustering, a higher number
+        generates more and smaller clusters, defaults to ``1.0``
+    :type resolution: float, optional
+    '''
+
+    if not ('distances' in data.obsp and 'connectivities' in data.obsp):
+        sc.pp.neighbors(data)
+
+    if alg == 'leiden':
+        sc.tl.leiden(data, resolution=resolution)
+
+    elif alg == 'louvain':
+        sc.tl.louvain(data, resolution=resolution)
+
+    else:
+        print('Algorithm not recognized, please use "leiden" or "louvain".')
