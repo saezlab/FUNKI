ce5628777c2ac70ff9a444783a60034306e2a3a0
Nicolas Palacio <nicolaspalacio91@gmail.com>
2024-02-28 13:01:42 +0100

    Added plot_tsne and plot_umap functions

--- src/funki/plots.py
+++ src/funki/plots.py
@@ -2,7 +2,7 @@ import scanpy as sc
 
 def plot_pca(data, color=None, use_highly_variable=True):
     '''
-    Plots the PCA results of a data set.
+    Plots the dimensionality reduction PCA results of a data set.
 
     :param data: The data set from which to compute the PCA
     :type data: :class:`funki.input.DataSet`
@@ -25,3 +25,58 @@ def plot_pca(data, color=None, use_highly_variable=True):
         sc.pp.highly_variable_genes(data, inplace=True)
 
     return sc.pl.pca(data, color=color)
+
+def plot_tsne(data, color=None, perplexity=30):
+    '''
+    Plots the dimensionality reduction t-SNE results of a data set.
+
+    :param data: The data set from which to compute the t-SNE
+    :type data: :class:`funki.input.DataSet`
+    :param color: Variables or observations to color from, defaults to ``None``
+    :type color: str | list[str], optional
+    :param perplexity: Perplexity hyperparmaeter for the t-SNE representation.
+        Relates to the number of nearest neighbours, defaults to ``30``
+    :type perplexity: int, optional
+    :returns: The figure contataining the resulting t-SNE
+    :rtype: `matplotlib.figure.Figure`_
+
+    .. _matplotlib.figure.Figure: https://matplotlib.org/stable/api/figure_api.\
+        html#matplotlib.figure.Figure
+    '''
+
+    if 'X_tsne' not in data.obsm:
+        sc.tl.tsne(data, perplexity=perplexity)
+
+    return sc.pl.tsne(data, color=color)
+
+def plot_umap(data, color=None, min_dist=0.5, spread=1.0, alpha=1.0, gamma=1.0):
+    '''
+    Plots the dimensionality reduction UMAP results of a data set.
+
+    :param data: The data set from which to compute the UMAP
+    :type data: :class:`funki.input.DataSet`
+    :param color: Variables or observations to color from, defaults to ``None``
+    :type color: str | list[str], optional
+    :param min_dist: Effective minimum distance between the embedded points
+    :type min_dist: float, optional
+    :param spread: Effective scale of embedded points
+    :type spread: float, optional
+    :param alpha: Initial learning rate for the optimization
+    :type alpha: float, optional
+    :param gamma: Weighting applied to negative samples for the optimization
+    :type gamma: float, optional
+    :returns: The figure contataining the resulting UMAP
+    :rtype: `matplotlib.figure.Figure`_
+
+    .. _matplotlib.figure.Figure: https://matplotlib.org/stable/api/figure_api.\
+        html#matplotlib.figure.Figure
+    '''
+
+    if not ('distances' in data.obsp and 'connectivities' in data.obsp):
+        sc.pp.neighbors(data)
+
+    if 'X_umap' not in data.obsm:
+        sc.tl.umap(data, min_dist=min_dist, spread=spread, alpha=alpha,
+                   gamma=gamma)
+
+    return sc.pl.umap(data, color=color)
