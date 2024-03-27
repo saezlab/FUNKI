7ce4aeedb2f0d6ef9ac6e616cbadf00b483ab5a5
Nicolas Palacio <nicolaspalacio91@gmail.com>
2024-03-27 16:26:00 +0100

    Implemented decoupler
    Basic wrapper over decoupler.decouple() function
    May add functionality to directly download prior knowledge per
    user request

--- src/funki/analysis.py
+++ src/funki/analysis.py
@@ -1,6 +1,50 @@
 import scanpy as sc
+import anndata as ad
+import decoupler as dc
 
 from .input import DataSet
+
+def enrich(data, net, methods=None, **kwargs):
+    '''
+    Performs enrichment analysis using `Decoupler`_ based on a given network
+    (e.g. gene set collection) and statistical method(s).
+
+    :param data: The data set from which to perform the enrichment
+    :type data: :class:`funki.input.DataSet`
+    :param net: The network linking the features of the data to the attributes
+        (e.g. pathways, gene sets, transcription factors, etc.)
+    :type net: `pandas.DataFrame`_
+    :param methods: Which statistical method(s) to use in order to compute the
+        enrichment, defaults to ``None``. If none is provided, uses ``'mlm'``,
+        ``'ulm'`` and ``'wsum'``. The option ``'all'`` performs all methods. To
+        see all the available methods, you can run `decoupler.show_methods()`_
+        function
+    :type methods: NoneType | str | list[str]
+    :param \*\*kwargs: Other keyword arguments that passed to
+    `decoupler.decouple()`_ function
+    :type \*\*kwargs: optional
+
+    :returns: ``None``, results are stored inplace of the passed ``data`` object
+    :rtype: NoneType
+
+    .. _Decoupler: https://decoupler-py.readthedocs.io/en/latest/index.html
+    .. _pandas.DataFrame: https://pandas.pydata.org/docs/reference/api/pandas.D\
+        ataFrame.html
+    .. _decoupler.show_methods(): https://decoupler-py.readthedocs.io/en/latest\
+        /generated/decoupler.show_methods.html#decoupler.show_methods
+    .. _decoupler.decouple(): https://decoupler-py.readthedocs.io/en/latest/gen\
+        erated/decoupler.decouple.html#decoupler.decouple
+    '''
+
+    # Making copy as AnnData to bypass Decoupler type checks
+    aux = ad.AnnData(data.copy())
+
+    dc.decouple(aux, net, methods=methods, use_raw=False, **kwargs)
+    
+    # Updating back the results to the original DataSet object
+    data.obsm.update(aux.obsm)
+
+
 
 def sc_trans_qc_metrics(data, var_name='mito'):
     '''
@@ -44,6 +88,9 @@ def sc_clustering(data, alg='leiden', resolution=1.0, **kwargs):
     :param \*\*kwargs: Other keyword arguments that passed to
         `scanpy.pp.neighbors()`_ function
     :type \*\*kwargs: optional
+
+    :returns: ``None``, results are stored inplace of the passed ``data`` object
+    :rtype: NoneType
 
     .. _scanpy.pp.neighbors(): https://scanpy.readthedocs.io/en/latest/api/gene\
         rated/scanpy.pp.neighbors.html
