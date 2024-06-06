606ae411c333e3be812afc9d264941d6e523d76c
Nicolas Palacio <nicolaspalacio91@gmail.com>
2024-06-06 13:31:18 +0200

    Added global color palette variable

--- src/funki/__init__.py
+++ src/funki/__init__.py
@@ -4,3 +4,10 @@ from . import plots
 from . import preprocessing
 
 __version__ = '0.0.2'
+_colors = {
+    'red': '#eb3f1b'
+    'yellow': '#f6c510'
+    'aqua': '#12cbd0'
+    'teal': '#09a687'
+    'blue': '#007fa0'
+}
