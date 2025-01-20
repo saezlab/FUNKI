ddca01d29f06119dda49a8a5a22003a2c57ddb41
Nicolàs Palacio-Escat <22887412+npalacioescat@users.noreply.github.com>
2025-01-20 10:52:59 +0100

    Update analysis.enrich default parameters

--- src/funki/analysis.py
+++ src/funki/analysis.py
@@ -11,8 +11,8 @@ def enrich(
     data,
     net,
     methods=None,
-    source=None,
-    target=None,
+    source='source',
+    target='target',
     weight=None,
     **kwargs
 ):
@@ -32,11 +32,11 @@ def enrich(
         function
     :type methods: NoneType | str | list[str]
     :param source: Column name from the provided ``net`` containing the gene
-        sets to enrich for.
+        sets to enrich for. Defaults to ``'source'``.
     :type source: str
     :param target: Column name from the provided ``net`` containing the gene set
         components (e.g. gene/protein names) that can be mapped back to the data
-        set variable names.
+        set variable names. Defaults to ``'target'``.
     :type target: str
     :param weight: Defines the column in the network containing the weights to
         use in the enrichment, defaults to ``None``.
