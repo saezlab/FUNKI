cc34dc109be9f9a152d48fce506ed638f40aad87
Nicolas Palacio <nicolaspalacio91@gmail.com>
2025-06-03 16:56:18 +0200

    Added running loop to app

--- src/funki/app/main.py
+++ src/funki/app/main.py
@@ -6,4 +6,11 @@ class Funki(tk.Frame):
     def __init__(self, root=None, **options):
         super().__init__(root)
         self.root = root
-        self.root.title('FUNKI v%.1f' % __version__)
+        self.root.title('FUNKI v%s' % __version__)
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+
+    app = Funki(root=root)
+    app.mainloop()
