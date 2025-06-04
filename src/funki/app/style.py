347cb1fb7b3ac5093f1786e5e110662bce4c10c2
Nicolas Palacio <nicolaspalacio91@gmail.com>
2025-06-04 18:40:56 +0200

    Testing some added colors to tab selector
    Will change colors later but at least is working

--- src/funki/app/style.py
+++ src/funki/app/style.py
@@ -14,10 +14,24 @@ def load_style():
         'TFrame',
         background=_colors['white'],
     )
-#    style.configure(
-#        'TNotebook',
-#        background=_colors['white'],
-#    )
+    style.configure(
+        'TNotebook.Tab',
+        padding=(5, 5, 5, 5),
+        background=_colors['red']
+    )
+    style.map(
+        'TNotebook.Tab',
+        background=[
+            ('selected', _colors['blue']),
+            ('active', _colors['teal']),
+            ('disabled', _colors['teal'])
+        ],
+        foreground=[
+            ('selected', _colors['red']),
+            ('active', _colors['yellow']),
+            ('disabled', _colors['yellow'])
+        ],
+    )
 #    style.configure(
 #        'TLabel',
 #        background=_colors['white'],
