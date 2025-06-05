de7a1b4ef7d41abfecb3d1daf5180999a3fb1215
Nicolas Palacio <nicolaspalacio91@gmail.com>
2025-06-05 11:44:04 +0200

    Home tab now displays welcome text

--- src/funki/app/tabs/home.py
+++ src/funki/app/tabs/home.py
@@ -1,7 +1,20 @@
 import tkinter as tk
 from tkinter import ttk
 
+from utils import read_text
+from utils import WrapLabel
 
 class TabHome(ttk.Frame):
+
     def __init__ (self, parent, **options):
+
         super().__init__(parent, **options)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
+        maintext = WrapLabel(
+            self,
+            text=read_text('assets/msg_home.txt'),
+            anchor='nw',
+        )
+        maintext.grid(row=0, column=0, sticky='NSEW')
