a68690d7aa332d98499c892d0ac435527c7ec994
Nicolas Palacio <nicolaspalacio91@gmail.com>
2025-06-06 16:38:57 +0200

    Testing Table class

--- src/funki/app/tabs/data.py
+++ src/funki/app/tabs/data.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
 
+from utils import Table
+
+import pandas as pd
+import numpy as np
+
 
 class TabData(ttk.Frame):
 
@@ -8,6 +13,12 @@ class TabData(ttk.Frame):
 
         super().__init__(parent, **options)
         self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
         self.rowconfigure(0, weight=1)
+
+        df = pd.DataFrame(np.arange(12).reshape(3, 4), columns=list('ABCD'))
+
+        table_data = Table(self, df)
+        table_data.grid(row=0, column=0, sticky='NSEW')
 
         
