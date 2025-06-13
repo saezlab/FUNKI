93adf0ef0f6b1d2c9fc845b10ddea7fbc67cd5ff
Nicolas Palacio <nicolaspalacio91@gmail.com>
2025-06-13 16:21:58 +0200

    Removed old web app

--- src/funki/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from .utils import parse_contents
-from .utils import serial_to_dataframe
-from .utils import serial_to_dataset
-from .utils import dataframe_to_serial
-from .utils import dataset_to_serial
-from .utils import md_to_str
-from .utils import info
