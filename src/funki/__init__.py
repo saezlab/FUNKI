5538082e70773e712b117b4eb0021d5ab9ab565e
Nicolas Palacio <nicolaspalacio91@gmail.com>
2025-06-13 13:43:55 +0200

    Version bump v0.1.1 -> v0.1.5

--- src/funki/__init__.py
+++ src/funki/__init__.py
@@ -4,7 +4,7 @@ from . import plots
 from . import preprocessing
 from . import pipelines
 
-__version__ = '0.1.1'
+__version__ = '0.1.5'
 _colors = {
     'red': '#eb3f1b',
     'yellow': '#f6c510',
