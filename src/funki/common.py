120346c93a6671247b00ee3802771b751997b8ad
Nicolas Palacio <nicolaspalacio91@gmail.com>
2025-06-16 14:17:07 +0200

    Added `common.is_numeric` function

--- src/funki/common.py
+++ src/funki/common.py
@@ -9,3 +9,23 @@ _colors = {
     'gray': '#8a8a8a',
     'lightgray': "#DADADA",
 }
+
+
+def is_numeric(var):
+    '''
+    Checks whether a value is numerical or not.
+    '''
+
+    if hasattr(var, '__iter__') and type(var) is not str:
+
+        return all([is_numeric(i) for i in var])
+
+    try:
+
+        float(var)
+
+        return True
+
+    except ValueError:
+
+        return False
