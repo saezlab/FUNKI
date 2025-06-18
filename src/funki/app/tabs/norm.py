98d181182d7f7e6933f39fa339904440a1b20a21
Nicolas Palacio <nicolaspalacio91@gmail.com>
2025-06-18 12:14:43 +0200

    Implemented filtering in app

--- src/funki/app/tabs/norm.py
+++ src/funki/app/tabs/norm.py
@@ -4,8 +4,9 @@ from tkinter import ttk
 import matplotlib.pyplot as plt
 
 from funki.pipelines import sc_quality_control
+from funki.preprocessing import sc_trans_filter
 
-from utils import Figure
+from utils import Figure, LabeledWidget
 
 class TabNorm(ttk.Frame):
 
@@ -24,6 +25,80 @@ class TabNorm(ttk.Frame):
         self.rowconfigure(4, weight=0)
         self.rowconfigure(5, weight=1)
 
+        # Filter panel
+        title_filter = ttk.Label(
+            self,
+            text='Filtering:',
+            style='Title.TLabel',
+            width=20
+        )
+        title_filter.grid(row=0, column=0, sticky='NSWE')
+
+        self.max_genes = tk.IntVar()
+        self.max_genes.set(5000)
+        entry_max_genes = LabeledWidget(
+            self,
+            ttk.Entry,
+            'Max. genes per cell: ',
+            lpos='w',
+            wget_kwargs={
+                'textvariable': self.max_genes,
+                'validate': 'key',
+                'validatecommand': self.controller.check_num,
+                'width': 10,
+            }
+        )
+        entry_max_genes.grid(row=1, column=0, sticky='W')
+
+        self.min_genes = tk.IntVar()
+        self.min_genes.set(500)
+        entry_min_genes = LabeledWidget(
+            self,
+            ttk.Entry,
+            'Min. genes per cell: ',
+            lpos='w',
+            wget_kwargs={
+                'textvariable': self.min_genes,
+                'validate': 'key',
+                'validatecommand': self.controller.check_num,
+                'width': 10,
+            }
+        )
+        entry_min_genes.grid(row=2, column=0, sticky='W')
+
+        self.mito_pct = tk.IntVar()
+        self.mito_pct.set(5)
+        self.slider_mito = LabeledWidget(
+            self,
+            tk.Scale,
+            'Max. % mito. genes per cell: ',
+            lpos='n',
+            wget_kwargs={
+                'from_': 0,
+                'to': 100,
+                'orient': 'horizontal',
+                'variable': self.mito_pct,
+            }
+        )
+        self.slider_mito.grid(row=3, column=0, sticky='W')
+
+        self.button_apply_filter = ttk.Button(
+            self,
+            text='Apply filters',
+            command=self.apply_filter
+        )
+        self.button_apply_filter.grid(row=4, column=0, sticky='W')
+
+        # Normalization panel
+        title_norm = ttk.Label(
+            self,
+            text='Normalization:',
+            style='Title.TLabel',
+            width=20
+        )
+        title_norm.grid(row=0, column=1, sticky='NSWE')
+
+        # Figure
         self.fig_raw, self.ax_raw = plt.subplots(nrows=2, ncols=3)
 
         self.figframe_raw = Figure(self, self.fig_raw)
@@ -40,3 +115,17 @@ class TabNorm(ttk.Frame):
 
             sc_quality_control(self.controller.data, ax=self.ax_raw)
             self.figframe_raw._update()
+
+
+    def apply_filter(self, *ev):
+
+        if self.controller.data:
+
+            self.controller.data = sc_trans_filter(
+                self.controller.data,
+                min_genes=self.min_genes.get(),
+                max_genes=self.max_genes.get(),
+                mito_pct=self.mito_pct.get(),
+            )
+
+            self.controller._update()
