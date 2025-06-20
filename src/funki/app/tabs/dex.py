2e70311210b2d32c41d5856902a283aab01ff0cc
Nicolas Palacio <nicolaspalacio91@gmail.com>
2025-06-20 17:17:57 +0200

    Added selectors for logFC and pval thresholds
    also same previous minor refactoring in dex.py

--- src/funki/app/tabs/dex.py
+++ src/funki/app/tabs/dex.py
@@ -24,23 +24,27 @@ class TabDex(ttk.Frame):
         self.rowconfigure(1, weight=0)
         self.rowconfigure(2, weight=0)
         self.rowconfigure(3, weight=0)
-        self.rowconfigure(4, weight=1)
+        self.rowconfigure(4, weight=0)
+        self.rowconfigure(5, weight=1)
 
-        title_dex = ttk.Label(
+        ttk.Label(
             self,
             text='Differential expression:',
             style='Title.TLabel',
             width=20
-        )
-        title_dex.grid(row=0, columnspan=3, sticky='NSWE')
+        ).grid(row=0, columnspan=3, sticky='NSWE')
 
         # Contrast variable selector
+        self.contrast_var = tk.StringVar()
         self.combox_contrast_var = LabeledWidget(
             self,
             ttk.Combobox,
             'Select group variable for the contrast: ',
             lpos='n',
-            wget_kwargs={'state': 'disabled'},
+            wget_kwargs={
+                'state': 'disabled',
+                'textvariable': self.contrast_var,
+            },
             wget_grid_kwargs={'sticky': 'EW', 'weight': 1},
             label_grid_kwargs={'sticky': 'EW', 'weight': 0},
         )
@@ -50,12 +54,13 @@ class TabDex(ttk.Frame):
         # TODO: Handle multiple groups?
 
         # Contrast group A
+        self.groupA = tk.StringVar()
         self.combox_A = LabeledWidget(
             self,
             ttk.Combobox,
             'Group of samples to contrast from: ',
             lpos='n',
-            wget_kwargs={'state': 'disabled'},
+            wget_kwargs={'state': 'disabled', 'textvariable': self.groupA},
             wget_grid_kwargs={'sticky': 'EW', 'weight': 1},
             label_grid_kwargs={'sticky': 'EW', 'weight': 0},
         )
@@ -63,12 +68,13 @@ class TabDex(ttk.Frame):
         self.combox_A.wg.bind('<<ComboboxSelected>>', self._update)
 
         # Contrast group B
+        self.groupB = tk.StringVar()
         self.combox_B = LabeledWidget(
             self,
             ttk.Combobox,
             'Group of samples to contrast against: ',
             lpos='n',
-            wget_kwargs={'state': 'disabled'},
+            wget_kwargs={'state': 'disabled', 'textvariable': self.groupB},
             wget_grid_kwargs={'sticky': 'EW', 'weight': 1},
             label_grid_kwargs={'sticky': 'EW', 'weight': 0},
         )
@@ -85,6 +91,37 @@ class TabDex(ttk.Frame):
         )
         self.button_swap.image = icon
         self.button_swap.grid(row=2, column=1, sticky='SW')
+
+        # Thresholds
+        self.thr_logfc = tk.DoubleVar()
+        self.thr_logfc.set(1.0)
+        LabeledWidget(
+            self,
+            ttk.Entry,
+            'log(FC) threshold: ',
+            lpos='w',
+            wget_kwargs={
+                'textvariable': self.thr_logfc,
+                'validate': 'key',
+                'validatecommand': self.controller.check_num,
+                'width': 5,
+            }
+        ).grid(column=0, row=3, sticky='W')
+
+        self.thr_pval = tk.DoubleVar()
+        self.thr_pval.set(0.05)
+        LabeledWidget(
+            self,
+            ttk.Entry,
+            'P-value threshold: ',
+            lpos='w',
+            wget_kwargs={
+                'textvariable': self.thr_pval,
+                'validate': 'key',
+                'validatecommand': self.controller.check_num,
+                'width': 5,
+            }
+        ).grid(column=2, row=3, sticky='W')
 
         # DEX methods
         method_frame = ttk.Frame(self, borderwidth=1, relief='groove')
@@ -112,7 +149,7 @@ class TabDex(ttk.Frame):
             value='pydeseq2'
         ).grid(row=0, column=2, sticky='E')
 
-        method_frame.grid(row=3, columnspan=2, sticky='NSEW')
+        method_frame.grid(row=4, columnspan=2, sticky='NSEW')
 
         # Compute button
         self.button_compute = ttk.Button(
@@ -121,20 +158,20 @@ class TabDex(ttk.Frame):
             command=self.compute,
             state='disabled',
         )
-        self.button_compute.grid(row=3, column=2, sticky='SE')
+        self.button_compute.grid(row=4, column=2, sticky='SE')
 
         # Figure
         self.fig, self.ax = plt.subplots()
 
         self.figframe = Figure(self, self.fig)
-        self.figframe.grid(row=4, columnspan=3, sticky='NSWE')
+        self.figframe.grid(row=5, columnspan=3, sticky='NSWE')
 
 
     def _update(self, *ev): # TODO: Could probably split to simplify
 
         if self.controller.data and not self.controller.data.obs.empty:
 
-            obs_key = self.combox_contrast_var.wg.get()
+            obs_key = self.contrast_var.get()
 
             # Main combobox is empty
             if not obs_key:
@@ -154,7 +191,7 @@ class TabDex(ttk.Frame):
                         state='readonly',
                         values=obs_keys,
                     )
-                    self.combox_contrast_var.wg.set(obs_key)
+                    self.contrast_var.set(obs_key)
 
             # Main combobox not empty
             if obs_key:
@@ -163,10 +200,10 @@ class TabDex(ttk.Frame):
 
                 # A/B are empty or new obs_key
                 if (
-                    (not self.combox_A.wg.get() or not self.combox_B.wg.get())
+                    (not self.groupA.get() or not self.groupB.wg.get())
                     or (
-                        self.combox_A.wg.get() not in options
-                        or self.combox_A.wg.get() not in options
+                        self.groupA.get() not in options
+                        or self.groupB.get() not in options
                     )
                 ):
 
@@ -174,19 +211,19 @@ class TabDex(ttk.Frame):
                         state='readonly',
                         values=[n for i, n in enumerate(options) if i != 1],
                     )
-                    self.combox_A.wg.set(options[0])
+                    self.groupA.set(options[0])
 
                     self.combox_B.wg.configure(
                         state='readonly',
                         values=[n for i, n in enumerate(options) if i != 0],
                     )
-                    self.combox_B.wg.set(options[1])
+                    self.groupB.set(options[1])
 
                 # A/B not empty -> ensure mutual exclusivity
                 else:
 
-                    curA = self.combox_A.wg.get()
-                    curB = self.combox_B.wg.get()
+                    curA = self.groupA.get()
+                    curB = self.groupB.get()
 
                     self.combox_A.wg.configure(
                         values=[i for i in options if i != curB],
@@ -203,23 +240,23 @@ class TabDex(ttk.Frame):
 
     def swap_ab(self):
 
-        curA = self.combox_A.wg.get()
-        curB = self.combox_B.wg.get()
+        curA = self.groupA.get()
+        curB = self.groupB.get()
 
         if curA and curB:
 
-            obs_key = self.combox_contrast_var.wg.get()
+            obs_key = self.contrast_var.get()
 
             options = sorted(set(self.controller.data.obs[obs_key]))
 
             self.combox_A.wg.configure(
                 values=[i for i in options if i != curA],
             )
-            self.combox_A.wg.set(curB)
+            self.groupA.set(curB)
             self.combox_B.wg.configure(
                 values=[i for i in options if i != curB],
             )
-            self.combox_B.wg.set(curA)
+            self.groupB.set(curA)
 
 
     def compute(self):
@@ -227,9 +264,11 @@ class TabDex(ttk.Frame):
         self.ax.clear()
         differential_expression(
             self.controller.data,
-            self.combox_contrast_var.wg.get(),
-            self.combox_A.wg.get(),
-            self.combox_B.wg.get(),
+            self.contrast_var.get(),
+            self.groupA.get(),
+            self.groupB.get(),
+            logfc_thr=self.thr_logfc.get(),
+            fdr_thr=self.thr_pval.get(),
             method=self.method.get(),
             ax=self.ax,
         )
